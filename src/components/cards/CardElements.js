f6c8ae942b5733b1469479c47057ecb2d1cfd01e
Samuel V. Rourke <samuelvanrourke@gmail.com>
2021-03-22 10:06:20 -0400

    created card elements

--- src/components/cards/CardElements.js
+++ src/components/cards/CardElements.js
@@ -1,11 +1,26 @@
 import styled from "styled-components";
 
+const CardBody = styled.div``;
+
 const CardHeading = styled.h3``;
-
 const RequestCancel = styled.button``;
-
 const TimeDisplay = styled.p``;
-
 const AcceptButton = styled.button``;
-
 const DeclineButton = styled.button``;
+const AcquiredItemButton = styled.button``;
+const UnavailableItemButton = styled.button``;
+const EditItemButton = styled.button``;
+const TrashItemButton = styled.button``;
+
+export {
+  CardBody,
+  CardHeading,
+  RequestCancel,
+  TimeDisplay,
+  AcceptButton,
+  DeclineButton,
+  AcquiredItemButton,
+  UnavailableItemButton,
+  EditItemButton,
+  TrashItemButton
+};
